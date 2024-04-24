6fa7337d0984f1007c9656dd3fed7eab6ea35e01
Jasmit <138936262+Jasmit22@users.noreply.github.com>
2024-04-24 01:02:35 -0600

    check the about and contact directories

---
+++ src/app/about/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      About
+      <p>This is an example route. To see it go to localhost:3000/about</p>
+      <p>
+        To make a route, make a directory named whatever you want the route to
+        be
+      </p>
+      <p>Then make a file named page.js in that directory</p>
+    </div>
+  );
+};
+
+export default Page;

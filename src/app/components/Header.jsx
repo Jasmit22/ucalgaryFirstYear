3d6b8c4051f807002dabbe0681d5b278b233a530
Jasmit <138936262+Jasmit22@users.noreply.github.com>
2024-07-15 19:31:26 -0600

    no more funky monkey styling

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,42 +4,17 @@ import Image from "next/image";
 
 export default function header() {
   return (
-    <header>
-      <nav className="flex items-center justify-between bg-ucalgaryRed text-white py-4 px-4 sticky top-0 z-50 text-lg">
+    <header className="flex sticky top-0 shadow-lg z-50">
+      <nav className="flex items-center justify-between bg-ucalgaryRed text-white py-4 px-4 sticky top-0 z-50 text-lg w-full">
         <Link
           href="/"
           className="text-2xl font-bold flex gap-3 items-center ml-4"
         >
           <Image src={gradientLogo2} alt="CampusConnect Logo" width={50} />
-          <h1 className="hover:text-ucalgaryGold transition-all duration-150 ease-linear text-2xl font-bold">
+          <h1 className="hover:text-ucalgaryGold transition-all duration-150 ease-linear text-3xl font-bold">
             Campus Connect
           </h1>
         </Link>
-        <ul className="menu">
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <a>Stuff</a>
-            <ul className="submenu bg-ucalgaryRed mt-2">
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-        </ul>
       </nav>
     </header>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,50 +1,45 @@
 export default function Home() {
   return (
-    <main style={{ color: "white" }}>
-      <div className="flex-col justify-center text-center h-screen ">
-        <h1 className="text-4xl font-bold text-center mt-4">
-          Welcome to CampusConnect
+    <main>
+      <div className="flex-col justify-center text-center h-screen text-ucalgaryRed">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to Campus Connect
         </h1>
       </div>
-      <div className="flex-col justify-center text-center">
-        <h3 className="font-bold text-lg mb-4">
-          Color Palette (With Black and White)
-        </h3>
-        <table className="w-full border-collapse">
-          <tbody>
-            <tr>
-              <td className="bg-ucalgaryRed text-white p-4 font-bold">
-                ucalgaryRed
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-ucalgaryGold text-black p-4 font-bold">
-                ucalgaryGold
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-ucalgaryLightOrange text-black p-4 font-bold">
-                ucalgaryLightOrange
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-ucalgaryDarkOrange text-white p-4 font-bold">
-                ucalgaryDarkOrange
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-ucalgaryLightGrey text-black p-4 font-bold">
-                ucalgaryLightGrey
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-ucalgaryDarkGrey text-white p-4 font-bold">
-                ucalgaryDarkGrey
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <table className="w-full border-collapse flex-col justify-center text-center">
+        <tbody>
+          <tr>
+            <td className="bg-ucalgaryRed text-white p-4 font-bold">
+              ucalgaryRed
+            </td>
+          </tr>
+          <tr>
+            <td className="bg-ucalgaryGold text-black p-4 font-bold">
+              ucalgaryGold
+            </td>
+          </tr>
+          <tr>
+            <td className="bg-ucalgaryLightOrange text-black p-4 font-bold">
+              ucalgaryLightOrange
+            </td>
+          </tr>
+          <tr>
+            <td className="bg-ucalgaryDarkOrange text-white p-4 font-bold">
+              ucalgaryDarkOrange
+            </td>
+          </tr>
+          <tr>
+            <td className="bg-ucalgaryLightGrey text-black p-4 font-bold">
+              ucalgaryLightGrey
+            </td>
+          </tr>
+          <tr>
+            <td className="bg-ucalgaryDarkGrey text-white p-4 font-bold">
+              ucalgaryDarkGrey
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }

5f2614000f47c7ab053b002c8a6947e28a944505
Jasmit <138936262+Jasmit22@users.noreply.github.com>
2024-07-22 17:12:03 -0600

    transit and google config

--- src/app/components/GetRoute.jsx
+++ src/app/components/GetRoute.jsx
@@ -1,12 +1,32 @@
-import { useState } from "react";
+"use client";
+import { useState, useRef } from "react";
+import { useJsApiLoader, StandaloneSearchBox } from "@react-google-maps/api";
+
+const libraries = ["places"];
 
 export default function GetRoute() {
   const [transitData, setTransitData] = useState(null);
+  const inputRef = useRef(null);
+  const key = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
-  const fetchTransitData = async (position) => {
-    const fromPlace = `${position.coords.latitude},${position.coords.longitude}`;
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: key,
+    libraries,
+  });
+
+  const handleOnPlacesChanged = () => {
+    const places = inputRef.current.getPlaces();
+    if (places && places.length > 0) {
+      const place = places[0];
+      const fromPlace = `${place.geometry.location.lat()},${place.geometry.location.lng()}`;
+      getLocationAndFetchRoute(fromPlace);
+    }
+  };
+
+  const fetchTransitData = async (fromPlace, toPlace) => {
     const response = await fetch(
-      `/api/transit/get-route?fromPlace=${fromPlace}&toPlace=51.079812941757964, -114.13492437368151`
+      `/api/transit/get-route?fromPlace=${fromPlace}&toPlace=${toPlace}`
     );
     const data = await response.json();
     console.log(data);
@@ -17,18 +37,36 @@ export default function GetRoute() {
     console.log("Error: " + error.message);
   };
 
-  async function getLocation() {
+  async function getLocationAndFetchRoute(fromPlace) {
     if (navigator.geolocation) {
-      await navigator.geolocation.getCurrentPosition(fetchTransitData, error);
+      navigator.geolocation.getCurrentPosition((position) => {
+        const toPlace = `${position.coords.latitude},${position.coords.longitude}`;
+        fetchTransitData(fromPlace, toPlace);
+      }, error);
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
   }
 
   return (
-    <div className="w-96 h-40 shadow-xl border border-gray-200">
-      <button onClick={getLocation} className="bg-black">
-        Get Route
+    <div className="w-full h-full shadow-xl border border-gray-200">
+      {isLoaded && (
+        <StandaloneSearchBox
+          onLoad={(ref) => (inputRef.current = ref)}
+          onPlacesChanged={handleOnPlacesChanged}
+        >
+          <input
+            type="text"
+            className="w-full bg-white border border-gray-300 rounded-md px-2 py-1 text-gray-400"
+            placeholder="Type your address here..."
+          />
+        </StandaloneSearchBox>
+      )}
+      <button
+        onClick={() => handleOnPlacesChanged()}
+        className="bg-black text-white p-2 mt-2"
+      >
+        Get From My Location
       </button>
     </div>
   );
